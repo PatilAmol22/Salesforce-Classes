8d1e5bb1b07629bed8674a92e361d3c09ebaebb4
PatilAmol22 <patilamol22795@gmail.com>
2023-01-10 11:11:34 +0530

    initial commit

---
+++ force-app/main/default/lwc/demoTask/demoTask.js
@@ -0,0 +1,58 @@
+import { LightningElement } from 'lwc';
+import fetchAccountsId from '@salesforce/apex/DemoTaskAccount.fetchAccountsID'; 
+ 
+  
+const COLUMNS = [  
+    { label: 'Id', fieldName: 'Id' },     
+    {label:'Preview', type: "button-icon", typeAttributes: {  
+        iconName: 'utility:preview', 
+        name: 'View',  
+        title: 'View',  
+        disabled: false,  
+        value: 'view',  
+        iconPosition: 'left'  
+    } }
+];  
+
+
+export default class demoTask extends LightningElement{  
+  
+    accounts;  
+    error;  
+    columns = COLUMNS;
+    AccountId1;
+    isModalOpen =false;
+    isShow = false;
+
+    connectedCallback()
+    {
+        fetchAccountsId( )    
+        .then(result => {  
+
+            this.accounts = result;  
+
+        })  
+        .catch(error => {  
+
+            this.error = error;  
+
+        });  
+    }
+    
+    
+    callRowAction( event ) {  
+          
+        const recId =  event.detail.row.Id;  
+        const actionName = event.detail.action.name;  
+       console.log('recId:'+recId);
+        this.isShow = true;
+        this.AccountId1 = recId;
+    } 
+
+    handleClose(){
+        this.isShow = false;
+
+    }
+
+    
+}
